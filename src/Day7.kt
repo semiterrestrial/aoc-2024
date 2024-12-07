a4b62aa4bf4de1bf97fdad0092bd55879afb8134
Øystein Øvrebø <ovrebo@gmail.com>
2024-12-07 23:59:16 +0100

    Day 7 parallellized

--- src/Day7.kt
+++ src/Day7.kt
@@ -6,17 +6,16 @@ import kotlin.time.measureTime
 data class Equation(val result: Long, val inputs: List<Long>)
 enum class Operator { PLUS, MULTIPLY, CONCAT }
 
+fun List<Operator>.generateCombinations(n: Int): List<List<Operator>> {
+    if (n == 0) return listOf(emptyList())
+    val smallerCombinations = this.generateCombinations(n - 1)
+    return smallerCombinations.flatMap { combination ->
+        this.map { combination + it }
+    }
+}
+
 fun main() {
     val time = measureTime {
-
-        fun List<Operator>.generateCombinations(n: Int): List<List<Operator>> {
-            if (n == 0) return listOf(emptyList())
-            val smallerCombinations = this.generateCombinations(n - 1)
-            return smallerCombinations.flatMap { combination ->
-                this.map { combination + it }
-            }
-        }
-
         //parse input
         val lines = Files.lines(Paths.get("day-7-input.txt")).toList()
         val eqRegex = """(\d+):(.*)""".toRegex()
@@ -25,41 +24,34 @@ fun main() {
             Equation(result.toLong(), inputs.trim().split(" ").map { it.toLong() })
         }
 
-        val operators = listOf(Operator.PLUS, Operator.MULTIPLY)
-        //keep only equations that can be solved correctly
-        val result1 = equations.filter { equation ->
-            val inputs = equation.inputs
-            val combinations = operators.generateCombinations(inputs.size - 1)
-            combinations.map { combination ->
-                val calculated = inputs.foldIndexed(0L) { index, acc, n ->
-                    if (index == 0 || combination[index - 1] == Operator.PLUS) acc + n else acc * n
-                }
-                calculated == equation.result
-            }.any { it }
-        }.sumOf { it.result }
+        val result1 = calculate(equations,listOf(Operator.PLUS, Operator.MULTIPLY))
         println("Result 1: $result1")
 
-        val part2operators = listOf(Operator.PLUS, Operator.MULTIPLY, Operator.CONCAT)
-        val result2 = equations.filter { equation ->
-            val inputs = equation.inputs
-            val combinations = part2operators.generateCombinations(inputs.size - 1)
-            combinations.map { combination ->
-                val calculated = inputs.foldIndexed(0L) { index, acc, n ->
-                    if (index == 0) n
-                    else {
-                        when (combination[index - 1]) {
-                            Operator.PLUS -> acc + n
-                            Operator.MULTIPLY -> acc * n
-                            else -> ("$acc$n").toLong() //concatenate
-                        }
-                    }
-                }
-                calculated == equation.result
-            }.any { it }
-        }.sumOf { it.result }
+        val result2 = calculate(equations, listOf(Operator.PLUS, Operator.MULTIPLY, Operator.CONCAT))
         println("Result 2 $result2")
     }
     println("Time spent: $time seconds")
 }
 
-
+fun calculate(
+    equations: List<Equation>,
+    operators: List<Operator>
+): Long {
+    return equations.parallelStream().filter { equation ->
+        val inputs = equation.inputs
+        val combinations = operators.generateCombinations(inputs.size - 1)
+        combinations.map { combination ->
+            val calculated = inputs.foldIndexed(0L) { index, acc, n ->
+                if (index == 0) n
+                else {
+                    when (combination[index - 1]) {
+                        Operator.PLUS -> acc + n
+                        Operator.MULTIPLY -> acc * n
+                        else -> ("$acc$n").toLong() //concatenate
+                    }
+                }
+            }
+            calculated == equation.result
+        }.any { it }
+    }.toList().sumOf { it.result }
+}
