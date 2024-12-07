732e4c5094353c74d8d480355cf8a54dc1f65953
Øystein Øvrebø <ovrebo@gmail.com>
2024-12-08 00:06:39 +0100

    Day 7 cleaner

--- src/Day7.kt
+++ src/Day7.kt
@@ -2,7 +2,6 @@ import java.nio.file.Files
 import java.nio.file.Paths
 import kotlin.time.measureTime
 
-
 data class Equation(val result: Long, val inputs: List<Long>)
 enum class Operator { PLUS, MULTIPLY, CONCAT }
 
@@ -12,25 +11,6 @@ fun List<Operator>.generateCombinations(n: Int): List<List<Operator>> {
     return smallerCombinations.flatMap { combination ->
         this.map { combination + it }
     }
-}
-
-fun main() {
-    val time = measureTime {
-        //parse input
-        val lines = Files.lines(Paths.get("day-7-input.txt")).toList()
-        val eqRegex = """(\d+):(.*)""".toRegex()
-        val equations = lines.map { line ->
-            val (result, inputs) = eqRegex.matchEntire(line)!!.destructured
-            Equation(result.toLong(), inputs.trim().split(" ").map { it.toLong() })
-        }
-
-        val result1 = calculate(equations,listOf(Operator.PLUS, Operator.MULTIPLY))
-        println("Result 1: $result1")
-
-        val result2 = calculate(equations, listOf(Operator.PLUS, Operator.MULTIPLY, Operator.CONCAT))
-        println("Result 2 $result2")
-    }
-    println("Time spent: $time seconds")
 }
 
 fun calculate(
@@ -43,15 +23,30 @@ fun calculate(
         combinations.map { combination ->
             val calculated = inputs.foldIndexed(0L) { index, acc, n ->
                 if (index == 0) n
-                else {
-                    when (combination[index - 1]) {
-                        Operator.PLUS -> acc + n
-                        Operator.MULTIPLY -> acc * n
-                        else -> ("$acc$n").toLong() //concatenate
-                    }
+                else when (combination[index - 1]) {
+                    Operator.PLUS -> acc + n
+                    Operator.MULTIPLY -> acc * n
+                    else -> ("$acc$n").toLong() //concatenate
                 }
             }
             calculated == equation.result
         }.any { it }
     }.toList().sumOf { it.result }
+}
+
+fun main() {
+    val time = measureTime {
+        val lines = Files.lines(Paths.get("day-7-input.txt")).toList()
+        val eqRegex = """(\d+):(.*)""".toRegex()
+        val equations = lines.map { line ->
+            val (result, inputs) = eqRegex.matchEntire(line)!!.destructured
+            Equation(result.toLong(), inputs.trim().split(" ").map { it.toLong() })
+        }
+        val result1 = calculate(equations, listOf(Operator.PLUS, Operator.MULTIPLY))
+        println("Result 1: $result1")
+
+        val result2 = calculate(equations, listOf(Operator.PLUS, Operator.MULTIPLY, Operator.CONCAT))
+        println("Result 2 $result2")
+    }
+    println("Time spent: $time seconds")
 }
