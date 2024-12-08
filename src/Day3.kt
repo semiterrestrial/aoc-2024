d584cbfd0a63f299515f9bd52f02a03124b22732
Øystein Øvrebø <ovrebo@gmail.com>
2024-12-08 16:27:23 +0100

    better day 3

--- src/Day3.kt
+++ src/Day3.kt
@@ -1,24 +1,26 @@
 fun main() {
     val input = getInputAsString("day-3-input.txt")
-    val mulRegex = """mul\((\d{1,3}),(\d{1,3})\)|(do\(\))|(don't\(\))""".toRegex()
+    val mulRegexPart1 = """mul\((\d{1,3}),(\d{1,3})\)""".toRegex()
+    val mulRegexPart2 = """mul\((\d{1,3}),(\d{1,3})\)|(do\(\))|(don't\(\))""".toRegex()
+    val matches = mulRegexPart1.findAll(input)
 
-    val matches = mulRegex.findAll(input)
-    val count = matches.toList().size
-    println("Matches: $count")
-    var shouldParse = true
-    val multiplicatives = matches
+    val result1 = matches
         .map { it.groupValues }
         .sumOf { group ->
-            println(group[0])
-            if(group[0] != "do()" && group[0] != "don't()" && shouldParse) {
-                val first = group[1].toInt()
-                val second = group[2].toInt()
-                println("multiply $first $second")
+            group[1].toInt() * group[2].toInt()
+        }
+
+    var shouldParse = true
+    val result2 = mulRegexPart2.findAll(input)
+        .map { it.groupValues }
+        .sumOf { group ->
+            val wholeMatch = group[0]
+            if (shouldParse && wholeMatch != "do()" && wholeMatch != "don't()") {
                 group[1].toInt() * group[2].toInt()
             } else {
-                shouldParse = group[0] == "do()"
+                shouldParse = wholeMatch == "do()"
                 0
             }
         }
-    println("Part 1: $multiplicatives")
+    printResults(result1, result2)
 }
