4cc7d61c7a81b99f16939699e233b6712bcd6c2c
Øystein Øvrebø <ovrebo@gmail.com>
2024-12-08 15:47:25 +0100

    fixed a failing test

--- test/UtilsKtTest.kt
+++ test/UtilsKtTest.kt
@@ -10,14 +10,14 @@ class UtilsKtTest {
         val start = 4 to 4
         val startValue = input[start.second][start.first]
         assertEquals('0', startValue)
-        assertEquals('1', getValueInDirection(start, Direction.N, input))
-        assertEquals('2', getValueInDirection(start, Direction.NE, input))
-        assertEquals('3', getValueInDirection(start, Direction.E, input))
-        assertEquals('4', getValueInDirection(start, Direction.SE, input))
-        assertEquals('5', getValueInDirection(start, Direction.S, input))
-        assertEquals('6', getValueInDirection(start, Direction.SW, input))
-        assertEquals('7', getValueInDirection(start, Direction.W, input))
-        assertEquals('8', getValueInDirection(start, Direction.NW, input))
+        assertEquals('1', getValueInDirection(start, Direction.N, input)?.second)
+        assertEquals('2', getValueInDirection(start, Direction.NE, input)?.second)
+        assertEquals('3', getValueInDirection(start, Direction.E, input)?.second)
+        assertEquals('4', getValueInDirection(start, Direction.SE, input)?.second)
+        assertEquals('5', getValueInDirection(start, Direction.S, input)?.second)
+        assertEquals('6', getValueInDirection(start, Direction.SW, input)?.second)
+        assertEquals('7', getValueInDirection(start, Direction.W, input)?.second)
+        assertEquals('8', getValueInDirection(start, Direction.NW, input)?.second)
     }
 
 
