91105128b7dacdeef55f0e85972353bf5ecb46df
Øystein Øvrebø <ovrebo@gmail.com>
2024-12-08 15:12:06 +0100

    shorter day 1

--- src/Day1.kt
+++ src/Day1.kt
@@ -3,17 +3,15 @@ import java.nio.file.Paths
 
 fun main() {
     val lines = Files.lines(Paths.get("day-1-input.txt")).toList()
-    val numbers = """(\d+)\s+(\d+)""".toRegex()  //regex for the two numbers
-    val accumulator = mutableListOf<Int>() to mutableListOf<Int>() //accumulator is just two lists
-    lines.fold(accumulator) { acc, s ->
-        numbers.find(s)?.let {
-            acc.first.add(it.groups[1]!!.value.toInt())
-            acc.second.add(it.groups[2]!!.value.toInt())
+    val (firstList, secondList) = lines.map {  s ->
+        s.split("   ").let {
+            it[0].toInt() to it[1].toInt()
         }
-        acc
-    }.toList().map { it.sort() }
-    val differences = accumulator.first.zip(accumulator.second).sumOf { (first, second) -> first.coerceAtLeast(second) - first.coerceAtMost(second) }
-    val numberOfMatches = accumulator.first.map { firstVal -> accumulator.second.count { it == firstVal } }
-    val differencesByMatches = accumulator.first.zip(numberOfMatches).sumOf { (first, second) -> first * second }
-    println("Part 1 = $differences, Part 2 = $differencesByMatches")
+    }.unzip()
+    val firstSorted = firstList.sorted()
+    val secondSorted = secondList.sorted()
+    val differences = firstSorted.zip(secondSorted).sumOf { (first, second) -> first.coerceAtLeast(second) - first.coerceAtMost(second) }
+    val numberOfMatches = firstSorted.map { firstVal -> secondSorted.count { it == firstVal } }
+    val differencesByMatches =firstSorted.zip(numberOfMatches).sumOf { (first, second) -> first * second }
+    printResults(differences, differencesByMatches)
 }
